26797c22d28a326fe228455dacb3e8fbf17d0aa5
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-02-17 11:35:59 -0500

    Commits AR object imageviews

--- app/src/main/java/com/example/metaphysical/MainActivity.java
+++ app/src/main/java/com/example/metaphysical/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.metaphysical;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
@@ -34,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         fragment = (ArFragment)
+        fragment = (ArFragment)
                 getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
         fragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
             fragment.onUpdate(frameTime);
@@ -61,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     private boolean updateTracking() {
         Frame frame = fragment.getArSceneView().getArFrame();
         boolean wasTracking = isTracking;
@@ -91,7 +96,57 @@ public class MainActivity extends AppCompatActivity {
 
     private android.graphics.Point getScreenCenter() {
         View vw = findViewById(android.R.id.content);
-        return new android.graphics.Point(vw.getWidth()/2, vw.getHeight()/2);
+        return new android.graphics.Point(vw.getWidth() / 2, vw.getHeight() / 2);
+    }
+
+
+    private void initializeGallery() {
+        LinearLayout gallery = findViewById(R.id.gallery_layout);
+        ImageView andy = new ImageView(this);
+        andy.setImageResource(R.drawable.droid_thumb);
+        andy.setContentDescription("andy");
+        andy.setOnClickListener(view -> addsfbObject(Uri.parse("andy.sfb")));
+        gallery.addView(andy);
+
+        ImageView cabin = new ImageView(this);
+        cabin.setImageResource(R.drawable.cabin_thumb);
+        cabin.setContentDescription("Cabin");
+        cabin.setOnClickListener(view -> addsfbObject(Uri.parse("Cabin.sfb")));
+        gallery.addView(cabin);
+
+        ImageView house = new ImageView(this);
+        house.setImageResource(R.drawable.house_thumb);
+        house.setContentDescription("house");
+        house.setOnClickListener(view -> addsfbObject(Uri.parse("House.sfb")));
+        gallery.addView(house);
+
+        ImageView igloo = new ImageView(this);
+        igloo.setImageResource(R.drawable.igloo_thumb);
+        igloo.setContentDescription("igloo");
+        igloo.setOnClickListener(view -> addsfbObject(Uri.parse("igloo.sfb")));
+        gallery.addView(igloo);
+
+    }
+
+    private void addsfbObject(Uri model) {
+        Frame frame = fragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+        if (frame != null){
+            hits = frame.hitTest(pt.x,pt.y);
+            for (HitResult hit: hits){
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof  Plane &&
+                        ((Plane)  trackable).isPoseInPolygon(hit.getHitPose())){
+                    placeObject(fragment,hit.createAnchor(), model);
+                    break;
+                }
+            }
+        }
+
+    }
+
+    private void placeObject(ArFragment fragment, Anchor anchor, Uri model) {
     }
 
 }
